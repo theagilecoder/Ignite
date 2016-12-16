5d4ac8c258753d236b32a3e2e858fd5b5f2f6e92
Pushkar Singh <theagilecoder@gmail.com>
2016-12-16 05:18:35 +0000

    Add Item management to Admin page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,7 @@ class StaticPagesController < ApplicationController
   
   def import
   end
+  
+  def admin
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   end  
   root 'static_pages#landing'
   get 'static_pages/import'
+  get 'static_pages/admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
